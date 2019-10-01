abd7d1a4b93ab3f3aea63a7bb7c793743961ee88
athenaozanich <athenaozanich@gmail.com>
2019-09-30 21:04:22 -0400

    Sync Projects

---
+++ classes.js
@@ -0,0 +1,22 @@
+//overlay
+  -title
+  -button
+    -text
+
+//keyboard
+  -keyrows = 3
+    -buttons = 7,9,10
+      -letter
+
+//score
+  -score = var
+  -Wins = var
+  -losses = var
+
+//phrase
+  -words
+    -letters
+      -text
+
+//gameInstance
+  -state
